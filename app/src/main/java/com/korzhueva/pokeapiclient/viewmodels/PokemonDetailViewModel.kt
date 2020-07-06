43972b3dd84f2db2101f5750f418baf6664552e9
DariaKorzhueva <korzhueva.daria@gmail.com>
2020-07-06 20:38:26 +0700

    Fix fragment detail layout

--- app/src/main/java/com/korzhueva/pokeapiclient/viewmodels/PokemonDetailViewModel.kt
+++ app/src/main/java/com/korzhueva/pokeapiclient/viewmodels/PokemonDetailViewModel.kt
@@ -6,11 +6,24 @@ import com.korzhueva.pokeapiclient.models.PokemonItem
 
 class PokemonDetailViewModel(pokemon: PokemonItem, app: Application) : AndroidViewModel(app) {
     private val _selectedPokemon = MutableLiveData<PokemonItem>()
+    var weight = "Weight: "
+    var height = "Height: "
+    var type = "Type: "
+    var hp = "HP: "
+    var attack = "Attack: "
+    var defense = "Defense: "
 
     val selectedPokemon: LiveData<PokemonItem>
         get() = _selectedPokemon
 
     init {
         _selectedPokemon.value = pokemon
+
+        weight += pokemon.weight
+        height += pokemon.height
+        type += pokemon.type
+        hp += pokemon.hp
+        attack += pokemon.attack
+        defense += pokemon.defense
     }
 }
