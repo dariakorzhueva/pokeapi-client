60d8b7ae5573fa79d31b8cbcc289a166388755f4
DariaKorzhueva <korzhueva.daria@gmail.com>
2020-07-09 21:32:43 +0700

    Add scroll down

--- app/src/main/java/com/korzhueva/pokeapiclient/ui/PokeListFragment.kt
+++ app/src/main/java/com/korzhueva/pokeapiclient/ui/PokeListFragment.kt
@@ -1,25 +1,26 @@
 package com.korzhueva.pokeapiclient.ui
 
-import android.R
-import android.content.Intent
 import android.os.Bundle
-import android.view.*
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.korzhueva.pokeapiclient.adapters.PhotoGridAdapter
 import com.korzhueva.pokeapiclient.databinding.FragmentPokelistBinding
 import com.korzhueva.pokeapiclient.viewmodels.PokeListViewModel
 
 
-class PokeListFragment : Fragment(){
-    private val viewModel : PokeListViewModel by lazy{
+class PokeListFragment : Fragment() {
+    private val viewModel: PokeListViewModel by lazy {
         ViewModelProviders.of(this).get(PokeListViewModel::class.java)
     }
-
-    private var countItems = 0
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -31,7 +32,7 @@ class PokeListFragment : Fragment(){
 
         binding.viewModel = viewModel
 
-        binding.photoGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener{
+        binding.photoGrid.adapter = PhotoGridAdapter(PhotoGridAdapter.OnClickListener {
             viewModel.displayPokemonDetails(it)
         })
 
@@ -43,16 +44,38 @@ class PokeListFragment : Fragment(){
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
 
-                viewModel.loadMore()
+                val gridLayoutManager = recyclerView.layoutManager as GridLayoutManager?
 
-                countItems += 30
+                if (gridLayoutManager != null) {
+                    val visibleItemCount = gridLayoutManager.childCount
+                    val totalItemCount = gridLayoutManager.itemCount
+                    val pastVisiblesItems = gridLayoutManager.findFirstVisibleItemPosition()
 
-                recyclerView.adapter!!.notifyItemChanged(countItems, (viewModel.photoList.value!!.size)!!.minus(1))
+                    Log.d("pokemooooon","$visibleItemCount $totalItemCount $pastVisiblesItems")
+
+                    var countItems = 0
+
+                    //TODO: fix scroll condition
+
+                    if (pastVisiblesItems > countItems) {
+                        viewModel.loadMore()
+
+                        countItems += 30
+
+                        recyclerView.adapter!!.notifyItemChanged(
+                            countItems,
+                            (viewModel.photoList.value!!.size)!!.minus(1)
+                        )
+
+                        countItems += 9
+                    }
+                }
             }
+
         })
 
         viewModel.navigateToSelectedPokemon.observe(viewLifecycleOwner, Observer {
-            if ( it != null ) {
+            if (it != null) {
                 this.findNavController().navigate(PokeListFragmentDirections.actionShowDetail(it))
                 viewModel.displayPokemonDetailsComplete()
             }
